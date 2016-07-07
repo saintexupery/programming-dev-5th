285cd61e2ef2a9ac4d54837f941fe0c9f8752a08
Simon <saintexupery@snu.ac.kr>
2016-07-07 16:59:41 +0900

    initial commit

---
+++ p_rogramming_1/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
+from blog import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.post_list)
+]
